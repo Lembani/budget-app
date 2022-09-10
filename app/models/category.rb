05a080d8e062df19896db72f62c8ea54febd50d2
Lembani <Lembanisakala@gmail.com>
2022-09-10 09:57:04 +0200

    Update category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,21 +3,8 @@ class Category < ApplicationRecord
 
   belongs_to :user
 
-  has_many :entities, dependent: :destroy
+  has_many :deals, dependent: :destroy
 
   validates :name, presence: true
-
-  def self.icons
-    {
-      housing: 'house.png',
-      transportation: 'transport.png',
-      food: 'food.png',
-      education: 'education.png',
-      health: 'health.png',
-      utilities: 'utils.png',
-      insurance: 'insurance.png',
-      default: 'default.png'
-    }
-  end
 
 end
