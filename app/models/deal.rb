3791a2e2146d2de7a5bf709b8286fde388845e84
Lembani <Lembanisakala@gmail.com>
2022-09-10 09:57:13 +0200

    Update deal model

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -1,15 +1,9 @@
 class Deal < ApplicationRecord
   default_scope { order(created_at: :desc) }
-  after_initialize :init
 
   belongs_to :user
   belongs_to :category
 
-
   validates :name, :amount, presence: true
   validates :amount, numericality: { greater_than: 0 }
-
-  def init
-    self.amount ||= 0
-  end
 end
