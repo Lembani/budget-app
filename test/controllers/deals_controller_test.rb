d39165b5cc889dece8bcbddb3832603962cd8db5
Lembani <Lembanisakala@gmail.com>
2022-09-10 10:09:43 +0200

    Fix linter errors

--- test/controllers/deals_controller_test.rb
+++ test/controllers/deals_controller_test.rb
@@ -1,22 +1,22 @@
-require "test_helper"
+require 'test_helper'
 
 class DealsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get deals_index_url
     assert_response :success
   end
 
-  test "should get new" do
+  test 'should get new' do
     get deals_new_url
     assert_response :success
   end
 
-  test "should get create" do
+  test 'should get create' do
     get deals_create_url
     assert_response :success
   end
 
-  test "should get destroy" do
+  test 'should get destroy' do
     get deals_destroy_url
     assert_response :success
   end
